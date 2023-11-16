3baa1efeca0d614fd1b00c5e601af42d028c40dd
Manoj kumar <131514672+Manoj8222@users.noreply.github.com>
2023-11-17 04:58:05 +0530

    adding pages for routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import appwriteService from "../appwrite/confiig";
+import { Container, PostCard } from "../components";
+import { useSelector } from "react-redux";
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+  const status = useSelector((state) => state.auth.status);
+  useEffect(() => {
+    appwriteService.getPosts().then((post) => {
+      if (post) {
+        setPosts(post.documents);
+      }
+    });
+  }, []);
+  if (posts.length === 0 && !status) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                Login to read posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
+            </div>
+        </Container>
+    </div>
+)
+}
+
+export default Home;
